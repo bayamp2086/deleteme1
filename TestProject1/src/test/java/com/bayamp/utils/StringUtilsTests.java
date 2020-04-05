d01c4bcb272027ad41539fd8abf69d420df2d6da
Harish Appannagari <github@bayamp.com>
2020-04-04 23:13:32 -0700

    Add reverse method that uses StringBuffer

--- TestProject1/src/test/java/com/bayamp/utils/StringUtilsTests.java
+++ TestProject1/src/test/java/com/bayamp/utils/StringUtilsTests.java
@@ -1,4 +1,8 @@
 package com.bayamp.utils;
 
 public class StringUtilsTests {
+    public static String reverseString(String str) {
+        StringBuffer buff = new StringBuffer(str);
+        return buff.reverse().toString();
+    }
 }
