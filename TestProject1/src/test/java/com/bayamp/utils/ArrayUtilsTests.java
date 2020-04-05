bbce498091e5451d9bc2ec9b2bd830f0834084e8
Harish Appannagari <github@bayamp.com>
2020-04-05 00:19:36 -0700

    Added reverse Array

--- TestProject1/src/test/java/com/bayamp/utils/ArrayUtilsTests.java
+++ TestProject1/src/test/java/com/bayamp/utils/ArrayUtilsTests.java
@@ -6,4 +6,16 @@ public class ArrayUtilsTests {
     public static void sortArray(int[] array) {
         Arrays.sort(array);
     }
+
+    public static int[] reverseArray(int[] array) {
+        int[] myArray = new int[array.length];
+        int lastIndex = myArray.length - 1;
+        int counter  = 0;
+        for(int i = lastIndex;i>=0;i--) {
+            myArray[counter] = array[lastIndex];
+            counter++;
+        }
+
+        return myArray;
+    }
 }
