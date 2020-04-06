adf91c67b04c87885dd4e7875d983e3af9c8d7af
Harish Appannagari <github@bayamp.com>
2020-04-05 18:23:02 -0700

    Still working

--- TestProject1/src/test/java/com/bayamp/utils/StringUtilsTests.java
+++ TestProject1/src/test/java/com/bayamp/utils/StringUtilsTests.java
@@ -1,4 +1,8 @@
 package com.bayamp.utils;
 
 public class StringUtilsTests {
+
+    public static String reverseString(String originalString){
+        return new StringBuffer(originalString).reverse().toString();
+    }
 }
