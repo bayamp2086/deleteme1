5b9ef19a25e4888150cb8b4e1e3eea2c67c43dac
Harish Appannagari <github@bayamp.com>
2020-04-05 22:36:28 -0700

    Revert "Still working on StringUtils"
    This reverts commit 03434eb4b9416131924f3523b854cf9158370678.

--- TestProject1/src/main/java/com/bayamp/utils/StringUtils.java
+++ TestProject1/src/main/java/com/bayamp/utils/StringUtils.java
@@ -1,11 +1,4 @@
 package com.bayamp.utils;
 
 public class StringUtils {
-
-    public static String reverseString(String originalString) {
-        StringBuffer buff = new StringBuffer(originalString);
-
-        return buff.reverse().toString();
-    }
-
 }
