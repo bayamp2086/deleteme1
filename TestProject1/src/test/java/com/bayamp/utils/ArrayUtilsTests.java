1d7b9d70a0dd1992148ec4dc810df7ca47e92edd
Harish Appannagari <github@bayamp.com>
2020-04-05 18:17:26 -0700

    no message

--- TestProject1/src/test/java/com/bayamp/utils/ArrayUtilsTests.java
+++ TestProject1/src/test/java/com/bayamp/utils/ArrayUtilsTests.java
@@ -1,4 +1,21 @@
 package com.bayamp.utils;
 
+import java.util.Arrays;
+
 public class ArrayUtilsTests {
+    public static void sortArray(int[] array) {
+        Arrays.sort(array);
+    }
+
+    public static int[] reverseArray(int[] array) {
+        int[] myArray = new int[array.length];
+        int lastIndex = myArray.length - 1;
+        int counter  = 0;
+        for(int i = lastIndex;i>=0;i--) {
+            myArray[counter] = array[lastIndex];
+            counter++;
+        }
+
+        return myArray;
+    }
 }
